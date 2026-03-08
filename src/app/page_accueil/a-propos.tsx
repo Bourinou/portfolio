893ab39e70221398b6e7b73492bd7d685144fda8
Dimitri PAPE <dimitri.pape@etu.umontpellier.fr>
2026-03-08 23:08:51 +0100

    feat: modification du texte à propos

--- src/app/page_accueil/a-propos.tsx
+++ src/app/page_accueil/a-propos.tsx
@@ -5,11 +5,17 @@ export default function APropos() {
                 <h2 className="titre-section">À propos de moi</h2>
                 <div className="contenu-a-propos">
                     <div className="texte-a-propos">
-                        <p className="description-a-propos">Étudiant en 2ème année de BUT informatique à l'IUT de Montpellier. Je
-                            travaille sur de nombreux projets en équipe.</p>
-                        <p className="description-a-propos">Ayant pratiqué diverses activités (judo, équitation, moto cross,
-                            natation, escalade, piano), je suis rigoureux, autonome et motivé dans tout ce que
-                            j'entreprends.</p>
+                        <p className="description-a-propos">Étudiant en 2ème année de BUT Informatique à l&apos;IUT de Montpellier,
+                            j&apos;ai choisi cette voie parce que l&apos;informatique me permet de résoudre des problèmes concrets
+                            et de créer des outils utiles à partir de rien. Ce qui me motive, c&apos;est de voir une idée
+                            prendre forme dans du code, puis devenir quelque chose de fonctionnel et d&apos;utilisable.</p>
+                        <p className="description-a-propos">Je suis particulièrement passionné par le développement web :
+                            concevoir des interfaces, donner vie à des maquettes et rendre une expérience utilisateur fluide
+                            et agréable sont des aspects qui me plaisent énormément. C&apos;est un domaine où créativité et
+                            logique se rejoignent, et c&apos;est ce mélange qui me fascine.</p>
+                        <p className="description-a-propos">En dehors du code, j&apos;ai pratiqué de nombreuses activités sportives
+                            (judo, équitation, moto cross, natation, escalade, piano) qui m&apos;ont appris la discipline, la
+                            persévérance et l&apos;esprit d&apos;équipe. Ces qualités me sont précieuses en informatique, où rigueur et collaboration sont essentielles.</p>
                     </div>
                 </div>
             </div>
